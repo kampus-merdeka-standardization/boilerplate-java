8b514f06ba3a3830d371c92973c9d617bb8ec328
ajipur <fajrarisqulla@gmail.com>
2024-03-20 09:47:56 +0700

    add simple graphql snippet by using web mvc

--- simple-graphql-web-mvc/src/main/java/t/it/simplegraphql/controllers/SimpleController.java
+++ simple-graphql-web-mvc/src/main/java/t/it/simplegraphql/controllers/SimpleController.java
@@ -2,9 +2,7 @@ package t.it.simplegraphql.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import t.it.simplegraphql.dto.PongResponse;
 import t.it.simplegraphql.services.SimpleService;
 
@@ -14,7 +12,6 @@ public class SimpleController {
     private final SimpleService simpleService;
 
     @QueryMapping(name = "response")
-    @ResponseStatus(HttpStatus.CREATED)
     public PongResponse ping() {
         return simpleService.ping();
     }
