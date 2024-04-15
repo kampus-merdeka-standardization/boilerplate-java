6ded5afb9232b0ae4e385f52acce79c85df8f93d
Fajra Risqulla <fajrarisqulla@gmail.com>
2024-04-15 11:24:58 +0700

    add unit test for spring graphql client

--- simple-spring-client/src/test/java/t/it/simplespringclient/infrastructures/repositories/PingGraphQlRepositoryImplTest.java
+++ simple-spring-client/src/test/java/t/it/simplespringclient/infrastructures/repositories/PingGraphQlRepositoryImplTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.graphql.client.GraphQlClient;
 import org.springframework.graphql.client.HttpGraphQlClient;
 import reactor.core.publisher.Mono;
