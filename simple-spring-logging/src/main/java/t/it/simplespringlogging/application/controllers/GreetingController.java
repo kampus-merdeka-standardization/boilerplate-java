412b23ff3592abbd83d535463f8ee2bf1ae537c5
Fajra Risqulla <fajrarisqulla@gmail.com>
2024-04-20 15:26:41 +0700

    migrate all logging configurations from logback-spring.xml to application.properties

--- simple-spring-logging/src/main/java/t/it/simplespringlogging/application/controllers/GreetingController.java
+++ simple-spring-logging/src/main/java/t/it/simplespringlogging/application/controllers/GreetingController.java
@@ -3,10 +3,13 @@ package t.it.simplespringlogging.application.controllers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import t.it.simplespringlogging.application.responses.MetaResponse;
+import t.it.simplespringlogging.application.responses.WebResponse;
 import t.it.simplespringlogging.domain.services.GreetingService;
 
 import java.util.UUID;
@@ -18,11 +21,16 @@ public class GreetingController {
     private final GreetingService greetingService;
 
     @GetMapping(path = "/greeting/{name}")
-    public Mono<String> greeting(@PathVariable("name") String name) {
+    public Mono<WebResponse<String>> greeting(@PathVariable("name") String name) {
         MDC.put("traceId", UUID.randomUUID().toString());
+
         log.info("greeting {}", name);
         Mono<String> greeting = greetingService.doGreeting(name);
+        Mono<WebResponse<String>> webResponse = greeting.map(response -> WebResponse.<String>builder().data(response).meta(MetaResponse.builder()
+                .code(String.valueOf(HttpStatus.OK.value()))
+                .message(HttpStatus.OK.getReasonPhrase()).build()).build());
+
         MDC.remove("traceId");
-        return greeting;
+        return webResponse;
     }
 }
