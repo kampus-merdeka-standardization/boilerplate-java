c643015314235379c89d14d5fb25c2ec71a0d42a
Fajra Risqulla <fajrarisqulla@gmail.com>
2024-04-20 16:14:20 +0700

    add fully unit test examples fro SimpleService

--- simple-spring-rest-server/src/test/java/t/it/simplespringrestserver/domains/services/SimpleServiceTest.java
+++ simple-spring-rest-server/src/test/java/t/it/simplespringrestserver/domains/services/SimpleServiceTest.java
@@ -6,6 +6,7 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 import t.it.simplespringrestserver.domains.services.models.AddUser;
 import t.it.simplespringrestserver.domains.services.models.CurrentUserState;
+import t.it.simplespringrestserver.domains.services.models.UpdateUser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,6 +26,7 @@ class SimpleServiceTest {
     @Test
     void testGetGreeting() {
         final var dummyName = "cisnux";
+
         Mono<String> greeting = simpleService.getGreetingByName(dummyName);
 
         StepVerifier.create(greeting).expectNext("Hello, " + dummyName + "!")
@@ -34,10 +36,55 @@ class SimpleServiceTest {
     @Test
     void testAddUser() {
         final AddUser dummyAddUser = AddUser.builder().name("cisnux").age(20).build();
+
         Mono<CurrentUserState> currentUserState = simpleService.addUser(dummyAddUser);
 
         StepVerifier.create(currentUserState).assertNext(response -> {
             assertEquals("Hello, " + dummyAddUser.name() + " you are " + dummyAddUser.age() + " years old", response.message());
+            assertNotNull(response.message());
+        }).verifyComplete();
+    }
+
+    @Test
+    void testUpdateAllFields() {
+        final UpdateUser dummyUpdateUser = UpdateUser.builder()
+                .id("1")
+                .currentName("name")
+                .newName("new name")
+                .build();
+
+        Mono<CurrentUserState> currentUserState = simpleService.updateAllFields(dummyUpdateUser);
+
+        StepVerifier.create(currentUserState).assertNext(response -> {
+            assertEquals("Your name is replaced from " + dummyUpdateUser.currentName() + " to " + dummyUpdateUser.newName(), response.message());
+            assertNotNull(response.message());
+        }).verifyComplete();
+    }
+
+    @Test
+    void testUpdateSomeFields() {
+        final UpdateUser dummyUpdateUser = UpdateUser.builder()
+                .id("1")
+                .currentName("name")
+                .newName("new name")
+                .build();
+
+        Mono<CurrentUserState> currentUserState = simpleService.updateSomeFields(dummyUpdateUser);
+
+        StepVerifier.create(currentUserState).assertNext(response -> {
+            assertEquals("Your name is replaced to " + dummyUpdateUser.newName(), response.message());
+            assertNotNull(response.message());
+        }).verifyComplete();
+    }
+
+    @Test
+    void testDeleteUser() {
+        final String dummyId = "1";
+
+        Mono<CurrentUserState> currentUserState = simpleService.deleteUser(dummyId);
+
+        StepVerifier.create(currentUserState).assertNext(response -> {
+            assertEquals("Your data is deleted", response.message());
             assertNotNull(response.message());
         }).verifyComplete();
     }
