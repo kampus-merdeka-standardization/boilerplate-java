53ddde89f4341e4c3dfb0c35beb89e285869dc3b
Fajra Risqulla <fajrarisqulla@gmail.com>
2024-04-25 12:10:15 +0700

    add fail case for unit test at simple-spring-rest-server

--- simple-spring-rest-server/src/test/java/t/it/simplespringrestserver/applications/controllers/SimpleControllerTest.java
+++ simple-spring-rest-server/src/test/java/t/it/simplespringrestserver/applications/controllers/SimpleControllerTest.java
@@ -1,5 +1,6 @@
 package t.it.simplespringrestserver.applications.controllers;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,108 +34,177 @@ class SimpleControllerTest {
     @Autowired
     private WebTestClient webClient;
 
-    @Test
-    void testGreeting_Success() {
-        final String dummyName = "cisnux";
+    @Nested
+    class GreetingTest {
+        @Test
+        void testGreeting_Success() {
+            final String dummyName = "cisnux";
 
-        when(simpleService.getGreetingByName(dummyName)).thenReturn(Mono.just("Hi " + dummyName + "!"));
+            when(simpleService.getGreetingByName(dummyName)).thenReturn(Mono.just("Hi " + dummyName + "!"));
 
-        webClient.get().uri("/hello/" + dummyName).accept(MediaType.APPLICATION_JSON).exchange()
-                .expectStatus().isOk()
-                .expectBody(WebResponse.class).value(webResponse -> {
-                    assertEquals("Hi " + dummyName + "!", webResponse.data());
-                    assertEquals(String.valueOf(HttpStatus.OK.value()), webResponse.meta().code());
-                });
+            webClient.get().uri("/hello/" + dummyName).accept(MediaType.APPLICATION_JSON).exchange()
+                    .expectStatus().isOk()
+                    .expectBody(WebResponse.class).value(webResponse -> {
+                        assertEquals("Hi " + dummyName + "!", webResponse.data());
+                        assertEquals(String.valueOf(HttpStatus.OK.value()), webResponse.meta().code());
+                    });
 
-        verify(simpleService, times(1)).getGreetingByName(dummyName);
+            verify(simpleService, times(1)).getGreetingByName(dummyName);
+        }
+
+        @Test
+        void testGreeting_Fail() {
+            final String dummyName = "cisnux";
+
+            when(simpleService.getGreetingByName(dummyName)).thenThrow(RuntimeException.class);
+
+            webClient.get().uri("/hello/" + dummyName).accept(MediaType.APPLICATION_JSON).exchange()
+                    .expectStatus().is5xxServerError();
+
+            verify(simpleService, times(1)).getGreetingByName(dummyName);
+        }
     }
 
-    @Test
-    void testAddUser_Success() {
-        AddUser addUser = AddUser.builder().name("cisnux").age(20).build();
-        CurrentUserState currentUserState = CurrentUserState.builder()
-                .id(UUID.randomUUID().toString())
-                .message("Hello, " + addUser.name() + " you are " + addUser.age() + " years old")
-                .timestamp(Instant.now().getEpochSecond())
-                .build();
+    @Nested
+    class AddUserTest {
+        @Test
+        void testAddUser_Success() {
+            AddUser addUser = AddUser.builder().name("cisnux").age(20).build();
+            CurrentUserState currentUserState = CurrentUserState.builder()
+                    .id(UUID.randomUUID().toString())
+                    .message("Hello, " + addUser.name() + " you are " + addUser.age() + " years old")
+                    .timestamp(Instant.now().getEpochSecond())
+                    .build();
 
-        when(simpleService.addUser(any(AddUser.class))).thenReturn(Mono.just(currentUserState));
+            when(simpleService.addUser(any(AddUser.class))).thenReturn(Mono.just(currentUserState));
 
-        webClient.post().uri("/hello").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(addUser)).exchange().expectStatus().isCreated().expectBody(new ParameterizedTypeReference<WebResponse<CurrentUserState>>() {
-        }).value(webResponse -> {
-            CurrentUserState actualData = webResponse.data();
-            assertEquals(currentUserState.id(), actualData.id());
-            assertEquals(currentUserState.message(), actualData.message());
-            assertEquals(String.valueOf(HttpStatus.CREATED.value()), webResponse.meta().code());
-        });
+            webClient.post().uri("/hello").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(addUser)).exchange().expectStatus().isCreated().expectBody(new ParameterizedTypeReference<WebResponse<CurrentUserState>>() {
+            }).value(webResponse -> {
+                CurrentUserState actualData = webResponse.data();
+                assertEquals(currentUserState.id(), actualData.id());
+                assertEquals(currentUserState.message(), actualData.message());
+                assertEquals(String.valueOf(HttpStatus.CREATED.value()), webResponse.meta().code());
+            });
 
-        verify(simpleService, times(1)).addUser(any(AddUser.class));
+            verify(simpleService, times(1)).addUser(any(AddUser.class));
+        }
+
+        @Test
+        void testAddUser_Fail() {
+            AddUser addUser = AddUser.builder().name("cisnux").age(20).build();
+
+            when(simpleService.addUser(any(AddUser.class))).thenThrow(RuntimeException.class);
+
+            webClient.post().uri("/hello").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(addUser)).exchange().expectStatus().is5xxServerError();
+
+            verify(simpleService, times(1)).addUser(any(AddUser.class));
+        }
     }
 
-    @Test
-    void testUpdateUser_Success() {
-        UpdateUser updateUser = UpdateUser.builder().currentName("cisnux").newName("fajra").build();
+    @Nested
+    class UpdateUserTest {
+        @Test
+        void testUpdateUser_Success() {
+            UpdateUser updateUser = UpdateUser.builder().currentName("cisnux").newName("fajra").build();
 
-        CurrentUserState currentUserState = CurrentUserState.builder()
-                .id(UUID.randomUUID().toString())
-                .message("Your name is replaced from " + updateUser.currentName() + " to " + updateUser.newName())
-                .timestamp(Instant.now().getEpochSecond())
-                .build();
+            CurrentUserState currentUserState = CurrentUserState.builder()
+                    .id(UUID.randomUUID().toString())
+                    .message("Your name is replaced from " + updateUser.currentName() + " to " + updateUser.newName())
+                    .timestamp(Instant.now().getEpochSecond())
+                    .build();
 
-        when(simpleService.updateAllFields(any(UpdateUser.class))).thenReturn(Mono.just(currentUserState));
+            when(simpleService.updateAllFields(any(UpdateUser.class))).thenReturn(Mono.just(currentUserState));
 
-        webClient.put().uri("/hello").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(updateUser)).exchange().expectStatus().isOk().expectBody(new ParameterizedTypeReference<WebResponse<CurrentUserState>>() {
-        }).value(webResponse -> {
-            CurrentUserState actualData = webResponse.data();
-            assertEquals(currentUserState.id(), actualData.id());
-            assertEquals(currentUserState.message(), actualData.message());
-            assertEquals(String.valueOf(HttpStatus.OK.value()), webResponse.meta().code());
-        });
+            webClient.put().uri("/hello").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(updateUser)).exchange().expectStatus().isOk().expectBody(new ParameterizedTypeReference<WebResponse<CurrentUserState>>() {
+            }).value(webResponse -> {
+                CurrentUserState actualData = webResponse.data();
+                assertEquals(currentUserState.id(), actualData.id());
+                assertEquals(currentUserState.message(), actualData.message());
+                assertEquals(String.valueOf(HttpStatus.OK.value()), webResponse.meta().code());
+            });
 
-        verify(simpleService, times(1)).updateAllFields(any(UpdateUser.class));
+            verify(simpleService, times(1)).updateAllFields(any(UpdateUser.class));
+        }
+
+        @Test
+        void testUpdateUser_Fail() {
+            UpdateUser updateUser = UpdateUser.builder().currentName("cisnux").newName("fajra").build();
+
+            when(simpleService.updateAllFields(any(UpdateUser.class))).thenThrow(RuntimeException.class);
+
+            webClient.put().uri("/hello").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(updateUser)).exchange().expectStatus().is5xxServerError();
+
+            verify(simpleService, times(1)).updateAllFields(any(UpdateUser.class));
+        }
     }
 
-    @Test
-    void testUpdateUserById_Success() {
-        UpdateUser updateUser = UpdateUser.builder().newName("fajra").build();
+    @Nested
+    class UpdateUserByIdTest{
+        @Test
+        void testUpdateUserById_Success() {
+            UpdateUser updateUser = UpdateUser.builder().newName("fajra").build();
 
-        CurrentUserState currentUserState = CurrentUserState.builder()
-                .id(UUID.randomUUID().toString())
-                .message("Your name is replaced to " + updateUser.newName())
-                .timestamp(Instant.now().getEpochSecond())
-                .build();
+            CurrentUserState currentUserState = CurrentUserState.builder()
+                    .id(UUID.randomUUID().toString())
+                    .message("Your name is replaced to " + updateUser.newName())
+                    .timestamp(Instant.now().getEpochSecond())
+                    .build();
 
-        when(simpleService.updateSomeFields(any(UpdateUser.class))).thenReturn(Mono.just(currentUserState));
+            when(simpleService.updateSomeFields(any(UpdateUser.class))).thenReturn(Mono.just(currentUserState));
 
-        webClient.patch().uri("/hello/" + currentUserState.id()).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(updateUser)).exchange().expectStatus().isOk().expectBody(new ParameterizedTypeReference<WebResponse<CurrentUserState>>() {
-        }).value(webResponse -> {
-            CurrentUserState actualData = webResponse.data();
-            assertEquals(currentUserState.id(), actualData.id());
-            assertEquals(currentUserState.message(), actualData.message());
-            assertEquals(String.valueOf(HttpStatus.OK.value()), webResponse.meta().code());
-        });
+            webClient.patch().uri("/hello/" + currentUserState.id()).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(updateUser)).exchange().expectStatus().isOk().expectBody(new ParameterizedTypeReference<WebResponse<CurrentUserState>>() {
+            }).value(webResponse -> {
+                CurrentUserState actualData = webResponse.data();
+                assertEquals(currentUserState.id(), actualData.id());
+                assertEquals(currentUserState.message(), actualData.message());
+                assertEquals(String.valueOf(HttpStatus.OK.value()), webResponse.meta().code());
+            });
 
-        verify(simpleService, times(1)).updateSomeFields(any(UpdateUser.class));
+            verify(simpleService, times(1)).updateSomeFields(any(UpdateUser.class));
+        }
+
+        @Test
+        void testUpdateUserById_Fail() {
+            UpdateUser updateUser = UpdateUser.builder().newName("fajra").build();
+
+            when(simpleService.updateSomeFields(any(UpdateUser.class))).thenThrow(RuntimeException.class);
+
+            webClient.patch().uri("/hello/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromValue(updateUser)).exchange().expectStatus().is5xxServerError();
+
+            verify(simpleService, times(1)).updateSomeFields(any(UpdateUser.class));
+        }
     }
 
-    @Test
-    void testDeleteUserById_Success() {
-        CurrentUserState currentUserState = CurrentUserState.builder()
-                .id(UUID.randomUUID().toString())
-                .message("Your data is deleted")
-                .timestamp(Instant.now().getEpochSecond())
-                .build();
+    @Nested
+    class DeleteUserByIdTest {
+        @Test
+        void testDeleteUserById_Success() {
+            CurrentUserState currentUserState = CurrentUserState.builder()
+                    .id(UUID.randomUUID().toString())
+                    .message("Your data is deleted")
+                    .timestamp(Instant.now().getEpochSecond())
+                    .build();
 
-        when(simpleService.deleteUser(anyString())).thenReturn(Mono.just(currentUserState));
+            when(simpleService.deleteUser(anyString())).thenReturn(Mono.just(currentUserState));
 
-        webClient.delete().uri("/hello/" + currentUserState.id()).accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(new ParameterizedTypeReference<WebResponse<CurrentUserState>>() {
-        }).value(webResponse -> {
-            CurrentUserState actualData = webResponse.data();
-            assertEquals(currentUserState.id(), actualData.id());
-            assertEquals(currentUserState.message(), actualData.message());
-            assertEquals(String.valueOf(HttpStatus.OK.value()), webResponse.meta().code());
-        });
+            webClient.delete().uri("/hello/" + currentUserState.id()).accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(new ParameterizedTypeReference<WebResponse<CurrentUserState>>() {
+            }).value(webResponse -> {
+                CurrentUserState actualData = webResponse.data();
+                assertEquals(currentUserState.id(), actualData.id());
+                assertEquals(currentUserState.message(), actualData.message());
+                assertEquals(String.valueOf(HttpStatus.OK.value()), webResponse.meta().code());
+            });
 
-        verify(simpleService, times(1)).deleteUser(anyString());
+            verify(simpleService, times(1)).deleteUser(anyString());
+        }
+
+        @Test
+        void testDeleteUserById_Fail() {
+            when(simpleService.deleteUser(anyString())).thenThrow(RuntimeException.class);
+
+            webClient.delete().uri("/hello/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON).exchange().expectStatus().is5xxServerError();
+
+            verify(simpleService, times(1)).deleteUser(anyString());
+        }
     }
 }
