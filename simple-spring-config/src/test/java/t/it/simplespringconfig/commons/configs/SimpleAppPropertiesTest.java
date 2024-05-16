22a941ce0de97e130388ba8e8bfa01594b655404
Fajra Risqulla <fajrarisqulla@gmail.com>
2024-05-16 21:11:54 +0700

    implement mocking at SimpleAppPropertiesTest

--- simple-spring-config/src/test/java/t/it/simplespringconfig/commons/configs/SimpleAppPropertiesTest.java
+++ simple-spring-config/src/test/java/t/it/simplespringconfig/commons/configs/SimpleAppPropertiesTest.java
@@ -2,39 +2,19 @@ package t.it.simplespringconfig.commons.configs;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class SimpleAppPropertiesTest {
+    @Mock
     private SimpleAppProperties simpleAppProperties;
-
-    @BeforeEach
-    void setUp() throws NoSuchFieldException, IllegalAccessException {
-        simpleAppProperties = new SimpleAppProperties();
-        final var simpleAppPropertiesClass = simpleAppProperties.getClass();
-
-        Field name = simpleAppPropertiesClass.getDeclaredField("name");
-        name.setAccessible(true);
-        name.set(simpleAppProperties, "simple-spring-config");
-
-        Field port = simpleAppPropertiesClass.getDeclaredField("port");
-        port.setAccessible(true);
-        port.set(simpleAppProperties, 9111);
-
-        Field dbHost = simpleAppPropertiesClass.getDeclaredField("dbHost");
-        dbHost.setAccessible(true);
-        dbHost.set(simpleAppProperties, "localhost:3306");
-
-        Field dbUsername = simpleAppPropertiesClass.getDeclaredField("dbUsername");
-        dbUsername.setAccessible(true);
-        dbUsername.set(simpleAppProperties, "root");
-
-        Field dbPassword = simpleAppPropertiesClass.getDeclaredField("dbPassword");
-        dbPassword.setAccessible(true);
-        dbPassword.set(simpleAppProperties, "root");
-    }
 
     @Test
     void testInit() {
@@ -43,6 +23,12 @@ class SimpleAppPropertiesTest {
 
     @Test
     void testConfig() {
+        when(simpleAppProperties.getName()).thenReturn("simple-spring-config");
+        when(simpleAppProperties.getPort()).thenReturn(9111);
+        when(simpleAppProperties.getDbHost()).thenReturn("localhost:3306");
+        when(simpleAppProperties.getDbUsername()).thenReturn("root");
+        when(simpleAppProperties.getDbPassword()).thenReturn("root");
+
         assertEquals("simple-spring-config", simpleAppProperties.getName());
         assertEquals(9111, simpleAppProperties.getPort());
         assertEquals("localhost:3306", simpleAppProperties.getDbHost());
