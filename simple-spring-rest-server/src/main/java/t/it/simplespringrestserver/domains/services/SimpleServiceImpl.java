865fa75968b2f0db6f24492bc00215c1a7c852f5
Fajra Risqulla <fajrarisqulla@gmail.com>
2024-06-24 21:43:07 +0700

    move duplicate code String from SimpleServiceImpl.java to StringUtils.java

--- simple-spring-rest-server/src/main/java/t/it/simplespringrestserver/domains/services/SimpleServiceImpl.java
+++ simple-spring-rest-server/src/main/java/t/it/simplespringrestserver/domains/services/SimpleServiceImpl.java
@@ -2,6 +2,7 @@ package t.it.simplespringrestserver.domains.services;
 
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import t.it.simplespringrestserver.commons.utils.StringUtils;
 import t.it.simplespringrestserver.domains.services.models.AddUser;
 import t.it.simplespringrestserver.domains.services.models.UpdateUser;
 import t.it.simplespringrestserver.domains.services.models.CurrentUserState;
@@ -15,14 +16,14 @@ public class SimpleServiceImpl implements SimpleService {
 
     @Override
     public Mono<String> getGreetingByName(String name) {
-        return Mono.just("Hello, " + name + "!");
+        return Mono.just(StringUtils.helloGreeter(name) + "!");
     }
 
     @Override
     public Mono<CurrentUserState> addUser(AddUser addUser) {
         return Mono.just(CurrentUserState.builder()
                 .id(id)
-                .message("Hello, " + addUser.name() + " you are " + addUser.age() + " years old")
+                .message(StringUtils.helloGreeter(addUser.name()) + " you are " + addUser.age() + " years old")
                 .timestamp(Instant.now().getEpochSecond())
                 .build());
     }
@@ -31,7 +32,7 @@ public class SimpleServiceImpl implements SimpleService {
     public Mono<CurrentUserState> updateAllFields(UpdateUser updateUser) {
         return Mono.just(CurrentUserState.builder()
                 .id(id)
-                .message("Your name is replaced from " + updateUser.currentName() + " to " + updateUser.newName())
+                .message(StringUtils.COMMON_GREETER_UPDATE_MESSAGE + " from " + updateUser.currentName() + " to " + updateUser.newName())
                 .timestamp(Instant.now().getEpochSecond())
                 .build());
     }
@@ -40,7 +41,7 @@ public class SimpleServiceImpl implements SimpleService {
     public Mono<CurrentUserState> updateSomeFields(UpdateUser updateUser) {
         return Mono.just(CurrentUserState.builder()
                 .id(id)
-                .message("Your name is replaced to " + updateUser.newName())
+                .message(StringUtils.COMMON_GREETER_UPDATE_MESSAGE + " to " + updateUser.newName())
                 .timestamp(Instant.now().getEpochSecond())
                 .build());
     }
